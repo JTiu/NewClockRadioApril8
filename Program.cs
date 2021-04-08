7fafa734b75f0c73fa5fe333cf4cab362ba1f30d
James Tiu <jamestiulawyer@gmail.com>
2021-04-08 14:45:07 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NewClockRadioApril8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Created April 8, 2021");
+            Console.ReadLine();
+
+        }
+    }
+}
